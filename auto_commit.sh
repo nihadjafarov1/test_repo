d20c7ce0077338cb864d0b54a118d0f7a126b7c1
Nihad Jafarov <70762257+nihadjafarov1@users.noreply.github.com>
2024-11-12 20:57:13 +0400

    Add 12.txt

--- auto_commit.sh
+++ auto_commit.sh
@@ -8,14 +8,15 @@ staged_files=$(git diff --name-only --cached)
 
 # Commit each modified, deleted, and new file individually
 for file in $staged_files; do
-    if git diff --cached --diff-filter=A --quiet -- "$file"; then
+    if git diff --cached --diff-filter=A --name-only -- "$file"; then
         commit_message="Add $file"
-    elif git diff --cached --diff-filter=M --quiet -- "$file"; then
+    elif git diff --cached --diff-filter=M --name-only -- "$file"; then
         commit_message="Update $file"
-    elif git diff --cached --diff-filter=D --quiet -- "$file"; then
+    elif git diff --cached --diff-filter=D --name-only -- "$file"; then
         commit_message="Delete $file"
     fi
     
     # Commit the file with the appropriate message
     git commit -m "$commit_message"
 done
+
